95c00caaca3120d8f184d2a8146f6a809e6bd87c
naorovel <32648183+naorovel@users.noreply.github.com>
2024-07-03 15:52:31 -0600

    finished first draft of the server + got vite/vue app to run in Docker container

--- tobii-ros/testscripts/main.py
+++ tobii-ros/testscripts/main.py
@@ -1,76 +1,77 @@
-import uvicorn
-import json
-import asyncio 
-from fastapi import FastAPI, Request, WebSocket
-from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
 from tobii_stream_engine import Api, Device, GazePoint, EyePosition, Stream, GazeOrigin, GazePoint, Stream, UserPresence, get_api_version
-from starlette.responses import StreamingResponse
-from fastapi.testclient import TestClient
+from flask import Flask, render_template, session
+from flask_socketio import SocketIO, emit
 
-app = FastAPI()
 tobiiAPI = Api(); 
 
+app = Flask("GazeInteractions")
+async_mode = None
+socketio = SocketIO(app, async_mode=async_mode)
+
+gaze_point: GazePoint = None
+gaze_origin: GazeOrigin = None
+eye_position: EyePosition = None
+user_presence: UserPresence = None
+timestamp: int = 0
+
+prev_data: dict = {
+    "gaze_point": gaze_point,
+    "gaze_origin": gaze_origin,
+    "eye_position": eye_position,
+    "user_presence": user_presence
+}
+
+@app.route('/')
+def index():
+  return render_template('index.html', async_mode=socketio.async_mode)
+
+def emit_data(data: any):
+    global prev_data
+    prev_data.update(data)
+    socketio.emit("update", data)
+    print(prev_data)
+
+
 def on_gaze_point(timestamp: int, gaze_point: GazePoint) -> None:
-    # print(f"{gaze_point=}")
+    emit_data({"gaze_point": gaze_point, "timestamp": timestamp})
     return gaze_point
 
 def on_gaze_origin(timestamp: int, gaze_origin: GazeOrigin) -> None:
-    # print(f"{gaze_origin=}")
+    emit_data({"gaze_origin": gaze_origin, "timestamp": timestamp})
     return gaze_origin
 
-
 def on_eye_position(timestamp: int, eye_position: EyePosition) -> None:
-    # print(f"{eye_position=}")
+    emit_data({"eye_position": eye_position, "timestamp": timestamp})
     return eye_position
 
-
 def on_user_presence(timestamp: int, user_presence: UserPresence) -> None:
-    # print(f"{user_presence=}")
+    emit_data({"user_presence": user_presence, "timestamp": timestamp})
     return user_presence
 
-@app.get("/")
-async def root():
-     return {"message": "hello world"}
+device_urls = tobiiAPI.enumerate_local_device_urls()
 
-@app.websocket("/datapoints")
-async def datapoints(websocket: WebSocket):
-    await websocket.accept()
-    device_urls = tobiiAPI.enumerate_local_device_urls()
+if not len(device_urls):
+    print("no device found") 
+    Exception(ValueError) # Change error type here to be more descriptive - no device found
 
-    if not len(device_urls):
-    # print("no device found") 
-        Exception(ValueError) # Change error type here to be more descriptive - no device found
+device = Device(api=tobiiAPI, url=device_urls[0])
+supported_capabilities = device.get_supported_capabilities()
 
-    device = Device(api=tobiiAPI, url=device_urls[0])
-    supported_capabilities = device.get_supported_capabilities()
+supported_streams = device.get_supported_streams()
 
-    supported_streams = device.get_supported_streams()
-    print(f"{supported_streams=}")
+if Stream.GAZE_POINT in supported_streams:
+    device.subscribe_gaze_point(callback=on_gaze_point)
 
-    if Stream.GAZE_POINT in supported_streams:
-        gaze_point = device.subscribe_gaze_point(callback=on_gaze_point)
+if Stream.GAZE_ORIGIN in supported_streams:
+    device.subscribe_gaze_origin(callback=on_gaze_origin)
 
-    if Stream.GAZE_ORIGIN in supported_streams:
-        gaze_origin = device.subscribe_gaze_origin(callback=on_gaze_origin)
+if Stream.EYE_POSITION_NORMALIZED in supported_streams:
+    device.subscribe_eye_position(callback=on_eye_position)
 
-    if Stream.EYE_POSITION_NORMALIZED in supported_streams:
-        eye_position = device.subscribe_eye_position(callback=on_eye_position)
+if Stream.USER_PRESENCE in supported_streams:
+    device.subscribe_user_presence(callback=on_user_presence)
 
-    if Stream.USER_PRESENCE in supported_streams:
-        user_presence = device.subscribe_user_presence(callback=on_user_presence)
+device.run()
 
-    device.run()
-
-    payload = {
-        gaze_point: gaze_point,
-        gaze_origin: gaze_origin,
-        eye_position: eye_position, 
-        user_presence: user_presence
-    }
-
-    await websocket.send_json(payload)
-    await websocket.close()
-    # test_websocket()
-
-# test_websocket()
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
